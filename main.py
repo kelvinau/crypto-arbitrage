efbc9cdd65213d9c9ab40177a2bfdf5b0895c72e
Kelvin Au <kelvinau4413@hotmail.com>
2017-12-24 07:24:30 -0800

    Add argument for production mode

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ f.close()
 
 parser = argparse.ArgumentParser(description='Crypto Arbitrage')
 parser.add_argument('-m', '--mode', help='Arbitrage mode: triangular or exchange', required=True)
+parser.add_argument('-p', '--production', help='Production mode', action='store_true')
 args = parser.parse_args()
 
 engine = None
+isMockMode = True if not args.production else False
+
 if args.mode == 'triangular':
     from engines.triangular_arbitrage import CryptoEngineTriArbitrage
-    engine = CryptoEngineTriArbitrage(config['triangular'], True)
+    engine = CryptoEngineTriArbitrage(config['triangular'], isMockMode)
 elif args.mode == 'exchange':
     from engines.exchange_arbitrage import CryptoEngineExArbitrage
-    engine = CryptoEngineExArbitrage(config['exchange'], True)
+    engine = CryptoEngineExArbitrage(config['exchange'], isMockMode)
 else:
     print 'Mode {0} is not recognized'.format(args.mode)
 
